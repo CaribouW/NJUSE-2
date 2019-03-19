ae151b4e83b2ffaaf04e880359fbbf84925b8890
lfm <1071956678@qq.com>
2019-03-19 17:40:09 +0800

    319

--- web-server/src/main/java/com/example/main/controller/UserController.java
+++ web-server/src/main/java/com/example/main/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.example.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.main.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class UserController {
+    @Autowired
+    private LoginService loginService;
+
     @GetMapping("/hello")
     public Mono<String> hello() {   // 返回类型为Mono<String>
         return Mono.just("Welcome to reactive world ~");     // 使用Mono.just生成响应式数据
     }
+
+    //    @RequestMapping(value = "/sasa",method = RequestMethod.POST)
+    @PostMapping("/sasa")
+    public JSON dasda(@RequestBody JSONObject jsonObject) {
+        String s =
+                jsonObject.getString("name");
+        return null;
+    }
+
 }
--- web-server/src/main/java/com/example/main/service/LoginService.java
+++ web-server/src/main/java/com/example/main/service/LoginService.java
@@ -1,7 +1,13 @@
 package com.example.main.service;
 
+import com.example.main.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
+    @Autowired
+    RoleRepository roleRepository;
+
+
 }
