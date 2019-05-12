2fbb318b245c71a7d25def2917ab3b34a0c94dc5
“171250618” <171250618@smail.nju.edu.cn>
2019-05-12 23:43:24 +0800

    add axios

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-04-26 09:41:58
+ * @LastEditTime: 2019-05-12 23:41:48
+ * @LastEditors: Please set LastEditors
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -7,6 +14,14 @@ import router from './router'
 // element组件
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+/*
+ * @Description: add Axios to handle httpRequest
+ * @Author: Zhou
+ * @LastEditTime: 2019-05-12 23:40
+*/
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
 
 Vue.config.productionTip = false
 
