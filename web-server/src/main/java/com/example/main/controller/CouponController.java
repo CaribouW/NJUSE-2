3809d28dbb2196e11abfc7726165d5155239176f
lfm <1071956678@qq.com>
2019-05-13 15:14:53 +0800

    Update CouponController.java

--- web-server/src/main/java/com/example/main/controller/CouponController.java
+++ web-server/src/main/java/com/example/main/controller/CouponController.java
@@ -2,6 +2,7 @@ package com.example.main.controller;
 
 import com.example.main.repository.CouponRepository;
 import com.example.main.repository.MapperUserCouponRepository;
+import com.example.main.service.CouponService;
 import com.example.main.utils.IDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CouponController {
     @Autowired
-    private CouponRepository couponRepository;
-    @Autowired
-    private MapperUserCouponRepository userCouponRepository;
+    private CouponService couponService;
     @Autowired
     private IDUtils idUtils;
 
