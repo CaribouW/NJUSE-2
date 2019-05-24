fb0cf1d2391552ecdc9f65b27772cc2c69ff6be5
lfm <1071956678@qq.com>
2019-05-24 22:04:36 +0800

    aaa

--- client/src/views/homepage/quickTable.vue
+++ client/src/views/homepage/quickTable.vue
@@ -1,13 +1,133 @@
 <template>
-    $END$
+  <div class="table-main">
+    <el-table
+      :data="tableData"
+      border
+      class="table"
+      style="width: 100%;">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="movieName"
+        label="电影名称"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="票价"
+        width="70"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="category"
+        label="类型"
+        width="80"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="info"
+        label="座位剩余情况"
+        width="120"
+      ></el-table-column>
+      <el-table-column
+        prop="refund"
+        label="退改票"
+        width="150"
+      ></el-table-column>
+      <el-table-column
+        prop="btn"
+        width="100"
+      >
+        <el-button
+          type="text"
+          style="color: #cff9fe;"
+          @click="purchase()"
+        >立即购买</el-button>
+
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "quickTable"
-    }
+  export default {
+    name: "quickTable",
+    props: {
+      date: ''
+    },
+    data() {
+      return {
+        tableData: [
+          {
+            date: '12:00 - 14:00',
+            movieName: '阿丽塔:战斗天使',
+            price: '￥' + '45',
+            category: '普通3D',
+            info: '',
+            refund: '可改票不可退票',
+          }, {
+            date: '12:00 - 14:00',
+            movieName: '阿丽塔:战斗天使',
+            price: '￥' + '45',
+            category: '普通3D',
+            info: '',
+            refund: '可改票不可退票',
+          }, {
+            date: '12:00 - 14:00',
+            movieName: '阿丽塔:战斗天使',
+            price: '￥' + '45',
+            category: '普通3D',
+            info: '',
+            refund: '可改票不可退票',
+          }, {
+            date: '12:00 - 14:00',
+            movieName: '阿丽塔:战斗天使',
+            price: '￥' + '45',
+            category: '普通3D',
+            info: '',
+            refund: '可改票不可退票',
+          },{
+            date: '12:00 - 14:00',
+            movieName: '阿丽塔:战斗天使',
+            price: '￥' + '45',
+            category: '普通3D',
+            info: '',
+            refund: '可改票不可退票',
+          },
+
+
+        ]
+      }
+    },
+    methods: {
+      getDate: function () {
+
+      },
+      //Purchase the ticket
+      //Redirect to the movie form
+      purchase:function () {
+        console.log('buy ticket')
+      }
+    },
+    computed: {}
+  }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.table-main {
+  text-align: center;
+  font-family: "PingFangSC-Medium",serif;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 23px;
+  .table {
+    background-color: transparent;
+  }
+}
+.cell{
 
+}
 </style>
--- client/src/views/quick/index.vue
+++ client/src/views/quick/index.vue
@@ -1,3 +1,81 @@
 <template>
-  <div>快速购票</div>
+  <div class="quick-main">
+    <div class="quick-container">
+      <div class="sub-div">
+        <div class="navigater">
+          <span class="text">当前排片&nbsp;:</span>
+          <el-date-picker
+            v-model="date"
+            type="date"
+            placeholder="选择日期"
+            class="date">
+          </el-date-picker>
+        </div>
+        <div class="table">
+          <quick-table :date="this.date"></quick-table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="js">
+  import QuickTable from "../homepage/quickTable";
+  export default {
+    components: {QuickTable},
+    data() {
+      return {
+        date: '',
+      }
+    },
+    mounted() {
+    },
+    methods: {
+      check: function () {
+        console.log(this.date)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .quick-main {
+    padding-top: 1%;
+    font-family: "PingFangSC-Medium", serif;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 23px;
+    color: #cff9fe;
+
+    .quick-container {
+      background: url('../../assets/images/homepage/Assets.png') no-repeat;
+      background-size: 100% 100%;
+      min-height: 520px;
+      height: fit-content;
+      box-sizing: border-box;
+      padding: 4% 70px 0 70px;
+      .sub-div {
+        /*width: 90%;*/
+        margin: 0 auto;
+        .navigater {
+          width: 70%;
+          margin: 0 auto;
+          text-align: left;
+          .el-input--prefix .el-input__inner{
+            height: 35px;
+          }
+          .date {
+            margin-left: 25px;
+            border: solid #cff9fe;
+          }
+        }
+        .table {
+          width: 85%;
+          margin: 14px auto 25px;
+          overflow: hidden;
+        }
+      }
+    }
+  }
+
+</style>
