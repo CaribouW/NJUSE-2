27beefa9caa2b1ae2948b354700b2864460e987c
lfm <1071956678@qq.com>
2019-05-25 15:34:33 +0800

    初步实现login的model ， service 内容

---
+++ client/src/service/userService.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+const baseIP = 'http://localhost:18080';
+
+/**
+ * 获取用户信息
+ * */
+export function getUserInfo(uid) {
+  return axios.get('', {
+    params: {
+      userId: uid
+    },
+    withCredentials: true,
+    baseURL: baseIP
+  }).then(res => {
+    return res.data;
+  }).then(res => {
+    console.log(res)
+  });
+}
+
+/**
+ * 用户登录
+ * */
+export function userLogin({account, password}) {
+  return axios.post('/user/login', {
+    account: account,
+    password: password,
+  }, {
+    baseURL: baseIP,
+    withCredentials: true,
+    responseType: 'application/json'
+  }).then(res => {
+    return res.data;
+  }).then(res => {
+    if(res.status === 200){
+        return res.data
+    }else{
+        return res.status
+    }
+  })
+}
