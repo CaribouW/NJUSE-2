6bbee295728d921c3cf8601cbba2400fabae82f4
171250035 <954853048@qq.com>
2019-06-04 20:37:34 +0800

    会员信息 没写完

---
+++ client/src/views/VIP/VIPInfo.vue
@@ -0,0 +1,56 @@
+<template>
+    <vip_aside>
+        <div class="VIPInfo">
+            <div class="info_header">  
+                <b>会员信息</b>         
+            </div>
+            <div class="info_body">
+                <p class="info_level">会员等级：黄金会员</p>
+                <p class="info_balance">会员卡余额：¥ 1000</p>
+                <div class="record">
+                    <div class="divider"></div>
+                </div>
+            </div>
+        </div>
+    </vip_aside>
+</template>
+
+<script>
+import vip_aside from "../VIP/index.vue"
+export default {
+    name:"VIP",
+    components:{
+        vip_aside,
+    },
+    data(){
+        return{
+            tabPosition:'left',
+        }
+    }
+
+}
+</script>
+
+<style lang='scss'>
+.VIPInfo{
+  padding: 10px 10px 10px 20px;
+  &_rectangle{
+    background: url('../../assets/images/homepage/Assets.png') no-repeat;
+    background-size: 100% 100%;
+    height: 790px;
+    margin-bottom: 50px ;
+  }
+}
+.info_header{
+    font-size: 30px;
+    text-align: left;
+    margin-bottom: 10px;
+}
+.divider{
+    color: #ffffff;
+    height: 1px;
+}
+
+
+
+</style>
