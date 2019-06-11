3dc0659159c90ab9447facb0df01d2a5389d2d2c
lfm <1071956678@qq.com>
2019-06-11 22:08:54 +0800

    Auto stash before merge of "master" and "origin/master"

--- client/src/router/manager.js
+++ client/src/router/manager.js
@@ -1,12 +1,22 @@
 import hallManage from '@/views/manager/component/hallManage.vue'
-
+import refundManage from '@/views/manager/component/refundStrategy.vue';
+import movieRoleManage from '@/views/manager/component/MovieRoleManage';
 
 const childernManager = [
   {
     path: 'hallManage',
     name: 'hallManage',
     component: hallManage
+  }, {
+    path: 'refundManage',
+    name: 'refundManage',
+    component: refundManage
   },
+  {
+    path: 'movieRoleManage',
+    name: 'movieRoleManage',
+    component: movieRoleManage
+  }
 ];
 
 export {childernManager}
---
+++ client/src/views/manager/component/refundStrategy.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="refund-strategy-main">
+    <div class="header">
+      <div class="header-text">
+        <span>退票策略</span>
+      </div>
+      <div class="choose">
+
+      </div>
+      <el-divider direction="horizontal"></el-divider>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "refundStrategy"
+  }
+</script>
+
+<style lang="scss">
+  .refund-strategy-main {
+    color: #FFFFFF;
+    padding: 30px;
+    .header {
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+
+      .el-divider {
+        border: 1px solid #979797;
+      }
+
+      .header-text {
+        text-align: left;
+        padding-bottom: 2%;
+
+        & > span {
+          height: 28px;
+          font-size: 22px;
+          line-height: 28px;
+        }
+      }
+    }
+  }
+</style>
