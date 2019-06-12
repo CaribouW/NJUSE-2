f4c6ac57bdd2947398da21fe97f89093f2099963
yyp <981395882@qq.com>
2019-06-12 20:41:53 +0800

    改bug

--- client/src/views/homepage/index.vue
+++ client/src/views/homepage/index.vue
@@ -91,13 +91,18 @@
         // 翻页
       },
       selectMovie(id) {
-        // this.$store.dispatch('getUserInfo')
-        this.$router.push({
-          path: '/movie/detail',
-          query: {
-            movieId: id
-          }
-        })
+        if (sessionStorage.getItem('userId')) {
+          this.$router.push({
+            path: '/movie/detail',
+            query: {
+              movieId: id
+            }
+          })
+        } else {
+          this.$message.error('请先登录')
+          this.$router.push('/login')
+        }
+        
       },
     },
     mounted () {
