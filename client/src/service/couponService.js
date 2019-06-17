d3b86d7d8f5897d258ef054d973aff676d7f123e
“171250618” <171250618@smail.nju.edu.cn>
2019-06-17 22:43:27 +0800

    优惠策略的增删改查

--- client/src/service/couponService.js
+++ client/src/service/couponService.js
@@ -1,7 +1,61 @@
-
+// 管理员对优惠策略的增删改查
 import {dealResponse,baseIP,doDelete, doGet, doPost, doPut} from './baseService.js'
 
+// 添加优惠策略
+export function addActivity({movies,couponNumber,conditionPrice,discountPrice,endDate,startDate,couponName}){
+  return doPost({
+    url: '/coupon/strategy',
+    body: {
+      movies: movies,
+      couponNumber:couponNumber,
+      conditionPrice: conditionPrice,
+      discountPrice: discountPrice,
+      endDate: endDate,
+      startDate: startDate,
+      couponName: couponName
+    }
+  }).then(res => {
+    return dealResponse(res)
+  })
+}
 
+// 删除优惠策略
+export function deleteActivity({couponId}){
+  return doDelete({
+    url: "/coupon/strategy",
+    params: {
+      couponId: couponId
+    }
+  })
+}
+
+// 修改优惠策略
+export function modifyActivity({couponId,movies,couponNumber,conditionPrice,discountPrice,endDate,startDate,couponName}){
+  return doPut({
+    url: "/coupon/strategy",
+    body: {
+      couponId: couponId,
+      movies: movies,
+      couponNumber:couponNumber,
+      conditionPrice: conditionPrice,
+      discountPrice: discountPrice,
+      endDate: endDate,
+      startDate: startDate,
+      couponName: couponName
+    }
+  }).then(res => {
+    return dealResponse(res)
+  })
+}
+
+// 获取优惠策略列表
+export function getActivityList(){
+  return doGet({
+    url: '/coupon/strategy/list'
+  }).then(res => {
+    return dealResponse(res)
+  })
+}
 
 /**
  * 获取用户优惠券列表 
